52554ac15fc646328faee49a94b06ca940639ab8
Sonia Chauhan <s.chauhan@sanchalansoft.com>
2020-11-25 19:40:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webservice2';
+  urlPic = "https://i.ibb.co/52bryQw/pic.jpg"
+  urlPic2 = "https://i.ibb.co/52bryQw/pic.jpg"
+  urlflwr = "https://i.ibb.co/5nvBMRQ/flower.jpg"
+  urlflower2 = "https://i.ibb.co/5nvBMRQ/flower.jpg"
+  urlwhite = "https://i.ibb.co/VwzRCbb/white.png"
+  urlcheer = "https://i.ibb.co/KG2vGrw/cheer.jpg"
+  urlShout = "https://i.ibb.co/7CsxvY5/pic.jpg"
 }
